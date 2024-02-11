02131403f7e65074b1c76618e2db42a01502c342
Travis CI <travis@travis-ci.org>
2024-02-11 18:23:24 +0100

    update the player script & spawn places

--- player.gd
+++ player.gd
@@ -14,7 +14,7 @@ func _physics_process(delta):
 	else:
 		$HappyBoo.play_idle_animation()
 
-	const DAMAGE_RATE = 500.0
+	const DAMAGE_RATE = 5.0
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
